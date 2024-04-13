22096322b9e699e7883a02e8d1b0afd8286f9f3c
Luís Gabriel Nascimento Simas <luis.gabriel_thera@prestador.globo>
2024-04-13 16:05:35 -0300

    fix: Ajuste na opção de Menu que estava duplicada

--- DesafioFundamentos/Models/Menu.cs
+++ DesafioFundamentos/Models/Menu.cs
@@ -25,8 +25,7 @@ static class Menu
             Console.WriteLine("Digite a sua opção:");
             Console.WriteLine("1 - Cadastrar veículo");
             Console.WriteLine("2 - Remover veículo");
-            Console.WriteLine("3 - Listar veículos");
-            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("3 - Listar veículos");            
             Console.WriteLine("5 - Encerrar");
 
             switch (int.Parse(Console.ReadLine()))
